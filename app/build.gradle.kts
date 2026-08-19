6f3da4bfa050b3c78c12698e9144b98c9a726b3f
David Erdman <david@Davids-Mac-mini.local>
2026-08-19 13:37:51 -0400

    Bump version to 1.0.12 (versionCode 14)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.erdman.kofc6650"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.0.11"
+        versionCode = 14
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
